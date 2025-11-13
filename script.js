5deff38252674f9c966a28c0f90e2d1bd0d48e3a
abdumuminovabduhalil <abdumuminovabduhalil2000@gmail.com>
2025-11-13 10:09:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// let ism = prompt("ismingizni kiriting");
+// let title = document.createElement("h1");
+
+// title.bodyColor = ism;
+// document.body.appendChild(title);
+
+// let div = document.getElementById("div");
+
+// let textColor = prompt("Enter text color");
+
+// div.style.color = textColor;
+
+// let body = document.getElementById("body");
+
+// let bodyColor = prompt("hfgdfhg");
+// body.style.Color = backgroundcolor;
+
+let bg = prompt("Enter body background color:");
+
+let txt = prompt("Enter body text color:");
+
+document.body.style.backgroundColor = bg;
+document.body.style.color = txt;
